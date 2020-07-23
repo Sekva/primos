d8fb1e3165154d064b63e31c9022a1c2de659c27
MBaltz <mateusbaltazar9@gmail.com>
2020-07-23 19:28:48 -0300

    Mudanças - resolvendo links href=/ e readme update

--- grid/servidor/Pesquisa_prob1/index.php
+++ grid/servidor/Pesquisa_prob1/index.php
@@ -3,8 +3,8 @@
 
 <h1>Pesquisa Prob_1</h1>
 
-<br><a href="Pesquisa_prob1/criar.php">- Criar um novo processo</a>
-<br><a href="Pesquisa_prob1/requisitar.php">- Página de Requisição</a>
-<br><a href="Pesquisa_prob1/resultados.php">- Ver Resultados</a>
+<br><a href="/Pesquisa_prob1/criar.php">- Criar um novo processo</a>
+<br><a href="/Pesquisa_prob1/requisitar.php">- Página de Requisição</a>
+<br><a href="/Pesquisa_prob1/resultados.php">- Ver Resultados</a>
 <!-- <br><a href="./listaClientes.php">- Contribuidores</a> -->
 <!-- <br><a href="./criar.php">- Criar</a> -->
