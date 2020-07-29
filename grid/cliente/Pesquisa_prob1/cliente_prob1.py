620dae7f395230ec97f65dedcf8a10b1a25c6f33
MBaltz <mateusbaltazar9@gmail.com>
2020-07-29 16:56:12 -0300

    Suporte ao envio de várias respostas encontradas por trabalho

--- grid/cliente/Pesquisa_prob1/cliente_prob1.py
+++ grid/cliente/Pesquisa_prob1/cliente_prob1.py
@@ -34,28 +34,7 @@ def requisitar_prob1(url):
             count_tentativas += 1
 
 
-
-# Atualiza status do processo e envia resultados
-def enviar_prob1(url, status, res_requisitar ,res_processar = False):
-    url_enviar = url + 'enviar.php'
-    conteudoEnvio = ''
-    if(status == 1):
-        conteudoEnvio = {'atualizarStatus': '', 'id':res_requisitar['id'], 'status':'1', 'resposta':'false'}
-
-    elif(res_processar != False): # quando terminar de processar
-        if(res_processar['resposta'] == 'true'): # obteve resposta
-            conteudoEnvio = {
-                'atualizarStatus':'',
-                'id':res_requisitar['id'],
-                'status':'3',
-                'resposta':'true',
-                'p':res_processar['p'],
-                'q':res_processar['q'],
-                'resultado_calculo':res_processar['resultado_calculo']
-            }
-        else: # nao obteve resposta
-            conteudoEnvio = {'atualizarStatus': '', 'id':res_requisitar['id'], 'status':'3', 'resposta':'false'}
-
+def tentar_enviar(url_enviar, conteudoEnvio):
     count_tentativas = 1
     resultadoAttProcesso = requests.post(url_enviar, data = conteudoEnvio)
     while (resultadoAttProcesso.text[-2:] != 'ok'):
@@ -66,12 +45,46 @@ def enviar_prob1(url, status, res_requisitar ,res_processar = False):
         time.sleep(3)
         resultadoAttProcesso = requests.post(url_enviar, data = conteudoEnvio)
         count_tentativas += 1
-
     return True
 
 
+# Atualiza status do processo e envia resultados
+def enviar_prob1(url, status, res_requisitar, res_processar = False):
+    url_enviar = url + 'enviar.php'
+    conteudoEnvio = ''
+    if(status == 1):
+        conteudoEnvio = {'atualizarStatus': '', 'id':res_requisitar['id'], 'status':'1', 'resposta':'false'}
+        return tentar_enviar(url_enviar, conteudoEnvio)
+
+    # Caso de termino de processamento de trabalho (encontrando ou não resposta)
+    elif(res_processar != False): # quando terminar de processar
+        for resp_i in res_processar:
+            if(resp_i['resposta'] == 'true'): # obteve resposta
+                conteudoEnvio = {
+                    'atualizarStatus':'',
+                    'id':res_requisitar['id'],
+                    'status':'3',
+                    'resposta':'true',
+                    'p':resp_i['p'],
+                    'q':resp_i['q'],
+                    'resultado_calculo':resp_i['resultado_calculo']
+                }
+            else: # nao obteve resposta
+                conteudoEnvio = {'atualizarStatus': '', 'id':res_requisitar['id'], 'status':'3', 'resposta':'false'}
+
+            # Pois está dentro de um for, se colcoar direto o return, quando for True, vai encerrar a funcao
+            if(tentar_enviar(url_enviar, conteudoEnvio) == False):
+                return False # encerra a funcao no primeiro erro
+
+        return True # Ao sair do for e concluir todos os envios
+
+    return False # Por via das duvidas (caso chame a funcao de modo errado)
+
+
 # Responsável pelo processamento dos dados
-def processar_prob1(url, res_requisitar):
+def processar_prob1(url, res_requisitar, diretorio_dados):
+    diretorio_dados += 'Lista_A.list'
+
     sucesso_att = enviar_prob1(url, 1, res_requisitar)
     if(sucesso_att == False):
         return False
@@ -86,7 +99,7 @@ def processar_prob1(url, res_requisitar):
     max_q = int(q) + res_requisitar["k"]
 
     args = str(p) + "/" + str(max_p) + "/" + str(q) + "/" + str(max_q)
-    p = fn(c_short(1), c_char_p(args.encode("utf-8")), c_char_p("../../../listas/arquivinho".encode("utf-8")))
+    p = fn(c_short(1), c_char_p(args.encode("utf-8")), c_char_p(diretorio_dados.encode("utf-8")))
     resultado = str(p.decode("utf-8"))
 
     dicionarios_retorno = []
@@ -94,11 +107,10 @@ def processar_prob1(url, res_requisitar):
     # verifica se tem resultado e se não, já retorna daqui
     if not resultado:
         print("Sem resposta....")
-        return {"resposta": False, "p": 0, "q": 0, "resultado_calculo": 0}
+        return [{"resposta": False, "p": 0, "q": 0, "resultado_calculo": 0}]
 
     # separa as linhas do resultado
     linhas = resultado.split('\n')
-
 
     # pra cada linha do resultado, extrai o dados
     for resposta_achada in linhas:
@@ -112,25 +124,11 @@ def processar_prob1(url, res_requisitar):
         dicionario_da_resposta = {"resposta": 'true', "p": valor_p, "q": valor_q, "resultado_calculo": valor_r}
         dicionarios_retorno.append(dicionario_da_resposta)
 
-    # retornando repostas
-    # só dar um jeito de enviar esse array todo ao inves de só 1
-    print(dicionarios_retorno)
-    exit()
-    return dicionarios_retorno[0]
-
-    ######
-    #resultado = chamada do processamento
-    ######
-
-    # O retorno deve ser um dicionário contendo:
-    # [resposta, p, q, resultado_calculo]
-    # o campo resposta é um boolean
-        # (true para quando achar um resultado)
-        # [resposta:true, p:3, q:7, resultado_calculo:121]
-    # return resultado
+    # vetor de dicionarios
+    return dicionarios_retorno
 
 
-def trabalhar_prob1(url):
+def trabalhar_prob1(url, diretorio_dados):
 
     # Requisita um novo trabalho
     res_requisitar = requisitar_prob1(url)
@@ -138,11 +136,10 @@ def trabalhar_prob1(url):
         return False
 
     #Processa o trabalho requisitado
-    res_processar = processar_prob1(url, res_requisitar)
+    res_processar = processar_prob1(url, res_requisitar, diretorio_dados)
     if(res_processar == False):
         return False
 
     #Submete o trabalho processado
     res_enviar = enviar_prob1(url, 3, res_requisitar, res_processar)
     return res_enviar
-
